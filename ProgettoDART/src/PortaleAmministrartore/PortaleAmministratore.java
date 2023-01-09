3c6ac7a7b9e3e8f121f3dadb9b41088c3331d36a
Veolox <Marco Li Gammari@DESKTOP-97MN2JU.wind3.hub>
2023-01-09 12:28:22 +0100

    Aggiunta di AssumiImpiegato e PortaleAmministratore
    Signed-off-by: Veolox <Marco Li Gammari@DESKTOP-97MN2JU.wind3.hub>

---
+++ ProgettoDART/src/PortaleAmministrartore/PortaleAmministratore.java
@@ -0,0 +1,133 @@
+package PortaleAmministrartore;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Font;
+import java.awt.Insets;
+import javax.swing.JSeparator;
+import java.awt.Color;
+import javax.swing.JComboBox;
+import javax.swing.JToggleButton;
+import javax.swing.JFormattedTextField;
+import javax.swing.JMenuBar;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JMenu;
+import javax.swing.JList;
+import javax.swing.UIManager;
+
+public class PortaleAmministratore {
+
+	private JFrame frmPortaleAmministratore;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PortaleAmministratore window = new PortaleAmministratore();
+					window.frmPortaleAmministratore.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PortaleAmministratore() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmPortaleAmministratore = new JFrame();
+		frmPortaleAmministratore.setTitle("Portale Amministratore");
+		frmPortaleAmministratore.setBounds(100, 100, 1483, 804);
+		frmPortaleAmministratore.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmPortaleAmministratore.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnNewButton.setBackground(Color.LIGHT_GRAY);
+		btnNewButton.setForeground(Color.LIGHT_GRAY);
+		frmPortaleAmministratore.getContentPane().add(btnNewButton, BorderLayout.CENTER);
+		
+		JPanel panel = new JPanel();
+		frmPortaleAmministratore.getContentPane().add(panel, BorderLayout.WEST);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[] {30, 0, 180};
+		gbl_panel.rowHeights = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 20};
+		gbl_panel.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JLabel lblNewLabel = new JLabel("Nome");
+		lblNewLabel.setFont(new Font("Impact", Font.PLAIN, 25));
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel.gridx = 1;
+		gbc_lblNewLabel.gridy = 1;
+		panel.add(lblNewLabel, gbc_lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Cognome");
+		lblNewLabel_1.setFont(new Font("Impact", Font.PLAIN, 25));
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_1.gridx = 1;
+		gbc_lblNewLabel_1.gridy = 2;
+		panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Matricola");
+		lblNewLabel_2.setFont(new Font("Impact", Font.PLAIN, 20));
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel_2.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_2.gridx = 1;
+		gbc_lblNewLabel_2.gridy = 3;
+		panel.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		JSeparator separator = new JSeparator();
+		separator.setForeground(new Color(0, 0, 0));
+		GridBagConstraints gbc_separator = new GridBagConstraints();
+		gbc_separator.ipady = 1;
+		gbc_separator.ipadx = 1;
+		gbc_separator.fill = GridBagConstraints.BOTH;
+		gbc_separator.gridwidth = 4;
+		gbc_separator.insets = new Insets(1, 1, 5, 8);
+		gbc_separator.gridx = 0;
+		gbc_separator.gridy = 4;
+		panel.add(separator, gbc_separator);
+		
+		JButton btnNewButton_1 = new JButton("Gestione Personale");
+		btnNewButton_1.setBorder(null);
+		btnNewButton_1.setForeground(Color.BLACK);
+		btnNewButton_1.setFont(new Font("Arial", Font.PLAIN, 15));
+		btnNewButton_1.setToolTipText("");
+		btnNewButton_1.setBackground(new Color(240, 240, 240));
+		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
+		gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton_1.gridx = 1;
+		gbc_btnNewButton_1.gridy = 6;
+		panel.add(btnNewButton_1, gbc_btnNewButton_1);
+	}
+
+}
