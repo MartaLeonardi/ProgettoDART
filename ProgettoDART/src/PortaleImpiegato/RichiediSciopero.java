898abcee1e9b3befde471a7473068962858c4d51
Marta <leonardimarta08@gmail.com>
2023-01-10 17:50:53 +0100

    aggiornamento

--- ProgettoDART/src/PortaleImpiegato/RichiediSciopero.java
+++ ProgettoDART/src/PortaleImpiegato/RichiediSciopero.java
@@ -1,10 +1,27 @@
 package PortaleImpiegato;
 
 import java.awt.EventQueue;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.GridBagLayout;
+import javax.swing.JTextField;
+import java.awt.GridBagConstraints;
+import javax.swing.JFormattedTextField;
+import java.awt.Insets;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 
 public class RichiediSciopero extends JFrame {
 
@@ -36,6 +53,68 @@ public class RichiediSciopero extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		//setExtendedState(JFrame.MAXIMIZED_BOTH);	
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JLabel LogoImg = new JLabel("");
+		LogoImg.setHorizontalAlignment(SwingConstants.CENTER);
+		Image imgLogo = new ImageIcon (this.getClass().getResource("/logo.png")).getImage();
+		contentPane.setLayout(new BorderLayout(0, 0));
+		LogoImg.setIcon(new ImageIcon(imgLogo));
+		contentPane.add(LogoImg, BorderLayout.NORTH);
+		
+		JPanel panel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		contentPane.add(panel, BorderLayout.SOUTH);
+		
+		JButton IndietroButton = new JButton("INDIETRO");
+		//-------------------------------------------------------------------------------------------------EVENTO INDIETRO > PortaleImpiegato.java
+				IndietroButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+						PortaleImpiegato portaleImpiegato = new PortaleImpiegato();
+						portaleImpiegato.setVisible(true);
+
+					}
+				});
+		IndietroButton.setHorizontalAlignment(SwingConstants.LEFT);
+		IndietroButton.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+		panel.add(IndietroButton);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_2 = new JPanel();
+		panel_1.add(panel_2, BorderLayout.SOUTH);
+		
+		JButton btnNewButton = new JButton("Richiedi");
+		btnNewButton.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+		panel_2.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("RICHIESTA FERIE");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 30));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_1.add(lblNewLabel, BorderLayout.NORTH);
+		
+		JPanel panel_3 = new JPanel();
+		panel_1.add(panel_3, BorderLayout.CENTER);
+		GridBagLayout gbl_panel_3 = new GridBagLayout();
+		gbl_panel_3.columnWidths = new int[]{158, 315, 809, 0};
+		gbl_panel_3.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gbl_panel_3.columnWeights = new double[]{0.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_panel_3.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel_3.setLayout(gbl_panel_3);
+		
+		JComboBox comboBox = new JComboBox();
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox.gridx = 1;
+		gbc_comboBox.gridy = 1;
+		panel_3.add(comboBox, gbc_comboBox);
+		
+		
 	}
 
 }
