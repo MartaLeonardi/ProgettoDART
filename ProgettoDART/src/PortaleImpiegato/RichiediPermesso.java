43489f1e19f8b2fb0b6e06ca860c0fac71c4b2c4
Marta <leonardimarta08@gmail.com>
2023-01-12 13:08:59 +0100

    aggiornamento

--- ProgettoDART/src/PortaleImpiegato/RichiediPermesso.java
+++ ProgettoDART/src/PortaleImpiegato/RichiediPermesso.java
@@ -23,7 +23,11 @@ import javax.swing.JSeparator;
 import javax.swing.DefaultComboBoxModel;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.util.Date;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class RichiediPermesso extends JFrame {
 
@@ -124,15 +128,16 @@ public class RichiediPermesso extends JFrame {
 		gbc_lblNewLabel_1_2.gridy = 2;
 		panel_3_1.add(lblNewLabel_1_2, gbc_lblNewLabel_1_2);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Malattia", "Congedo parentale"}));
-		comboBox.setFont(new Font("Times New Roman", Font.PLAIN, 23));
+		JComboBox motivazioni = new JComboBox();
+		String[] listaMotivazioni= new String[] {"Malattia", "Congedo parentale"};
+		motivazioni.setModel(new DefaultComboBoxModel(listaMotivazioni));
+		motivazioni.setFont(new Font("Times New Roman", Font.PLAIN, 23));
 		GridBagConstraints gbc_comboBox = new GridBagConstraints();
 		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
 		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
 		gbc_comboBox.gridx = 2;
 		gbc_comboBox.gridy = 2;
-		panel_3_1.add(comboBox, gbc_comboBox);
+		panel_3_1.add(motivazioni, gbc_comboBox);
 		
 		JLabel lblNewLabel_2 = new JLabel(" ");
 		lblNewLabel_2.setFont(new Font("Times New Roman", Font.PLAIN, 25));
@@ -142,7 +147,7 @@ public class RichiediPermesso extends JFrame {
 		gbc_lblNewLabel_2.gridy = 3;
 		panel_3_1.add(lblNewLabel_2, gbc_lblNewLabel_2);
 		
-		JLabel lblNewLabel_1 = new JLabel("Scegli il giorno di inizio ferie:");
+		JLabel lblNewLabel_1 = new JLabel("Scegli il giorno di inizio permesso:");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblNewLabel_1.setFont(new Font("Times New Roman", Font.PLAIN, 25));
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
@@ -169,7 +174,7 @@ public class RichiediPermesso extends JFrame {
 		gbc_ProvaInserimentoInizio.gridy = 5;
 		panel_3_1.add(ProvaInserimentoInizio, gbc_ProvaInserimentoInizio);
 		
-		JLabel lblNewLabel_1_1 = new JLabel("Scegli il giorno di fine ferie:");
+		JLabel lblNewLabel_1_1 = new JLabel("Scegli il giorno di fine permesso:");
 		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblNewLabel_1_1.setFont(new Font("Times New Roman", Font.PLAIN, 25));
 		GridBagConstraints gbc_lblNewLabel_1_1 = new GridBagConstraints();
@@ -196,14 +201,15 @@ public class RichiediPermesso extends JFrame {
 		gbc_ProvaInserimentoFine.gridy = 7;
 		panel_3_1.add(ProvaInserimentoFine, gbc_ProvaInserimentoFine);
 		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("Giornate complete");
-		chckbxNewCheckBox.setFont(new Font("Times New Roman", Font.PLAIN, 25));
-		GridBagConstraints gbc_chckbxNewCheckBox = new GridBagConstraints();
-		gbc_chckbxNewCheckBox.anchor = GridBagConstraints.NORTH;
-		gbc_chckbxNewCheckBox.insets = new Insets(0, 0, 0, 5);
-		gbc_chckbxNewCheckBox.gridx = 1;
-		gbc_chckbxNewCheckBox.gridy = 8;
-		panel_3_1.add(chckbxNewCheckBox, gbc_chckbxNewCheckBox);
+		JCheckBox giornateComplete = new JCheckBox("Giornate complete");
+		
+		giornateComplete.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+		GridBagConstraints gbc_giornateComplete = new GridBagConstraints();
+		gbc_giornateComplete.anchor = GridBagConstraints.NORTH;
+		gbc_giornateComplete.insets = new Insets(0, 0, 0, 5);
+		gbc_giornateComplete.gridx = 1;
+		gbc_giornateComplete.gridy = 8;
+		panel_3_1.add(giornateComplete, gbc_giornateComplete);
 		
 		JPanel panel_3 = new JPanel();
 		panel_4.add(panel_3, BorderLayout.SOUTH);
@@ -213,42 +219,110 @@ public class RichiediPermesso extends JFrame {
 		lblNewLabel_3.setFont(new Font("Times New Roman", Font.PLAIN, 25));
 		panel_3.add(lblNewLabel_3);
 		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"}));
-		comboBox_1.setFont(new Font("Times New Roman", Font.PLAIN, 25));
-		panel_3.add(comboBox_1);
+		String[] ora = new String[] {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"};
+		String[] minuti = new String[] {"00", "15", "30", "45"};
+		
+		JComboBox oraInizio = new JComboBox();
+		oraInizio.setModel(new DefaultComboBoxModel(ora));
+		oraInizio.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+		panel_3.add(oraInizio);
 		
 		JLabel lblNewLabel_3_1 = new JLabel(":    Minuti inizio:");
 		lblNewLabel_3_1.setFont(new Font("Times New Roman", Font.PLAIN, 25));
 		panel_3.add(lblNewLabel_3_1);
 		
-		JComboBox comboBox_1_1 = new JComboBox();
-		comboBox_1_1.setModel(new DefaultComboBoxModel(new String[] {"00", "15", "30", "45"}));
-		comboBox_1_1.setFont(new Font("Times New Roman", Font.PLAIN, 25));
-		panel_3.add(comboBox_1_1);
+		JComboBox minutoInizio = new JComboBox();
+		minutoInizio.setModel(new DefaultComboBoxModel(minuti));
+		minutoInizio.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+		panel_3.add(minutoInizio);
 		
 		JLabel lblNewLabel_3_2 = new JLabel("-    Ora fine:");
 		lblNewLabel_3_2.setFont(new Font("Times New Roman", Font.PLAIN, 25));
 		panel_3.add(lblNewLabel_3_2);
 		
-		JComboBox comboBox_1_2 = new JComboBox();
-		comboBox_1_2.setModel(new DefaultComboBoxModel(new String[] {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"}));
-		comboBox_1_2.setFont(new Font("Times New Roman", Font.PLAIN, 25));
-		panel_3.add(comboBox_1_2);
+		JComboBox oraFine = new JComboBox();
+		oraFine.setModel(new DefaultComboBoxModel(ora));
+		oraFine.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+		panel_3.add(oraFine);
 		
 		JLabel lblNewLabel_3_1_1 = new JLabel(":    Minuti fine:");
 		lblNewLabel_3_1_1.setFont(new Font("Times New Roman", Font.PLAIN, 25));
 		panel_3.add(lblNewLabel_3_1_1);
 		
-		JComboBox comboBox_1_1_1 = new JComboBox();
-		comboBox_1_1_1.setModel(new DefaultComboBoxModel(new String[] {"00", "15", "30", "45"}));
-		comboBox_1_1_1.setFont(new Font("Times New Roman", Font.PLAIN, 25));
-		panel_3.add(comboBox_1_1_1);
+		JComboBox minutoFine = new JComboBox();
+		minutoFine.setModel(new DefaultComboBoxModel(minuti));
+		minutoFine.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+		panel_3.add(minutoFine);
 		
 		JPanel panel_5 = new JPanel();
 		panel_2.add(panel_5, BorderLayout.SOUTH);
 		
+		
+		giornateComplete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Boolean checkBoxGiornate = giornateComplete.isSelected();			// contenuto checkbox
+				
+				if(checkBoxGiornate== true)									//disattivazione delle combobox per la checkbox
+				{
+					oraInizio.setEnabled(false);
+					minutoInizio.setEnabled(false);
+					oraFine.setEnabled(false);
+					minutoFine.setEnabled(false);
+					
+				}else														//attivazione delle combobox per la checkbox
+				{
+					
+					oraInizio.setEnabled(true);
+					minutoInizio.setEnabled(true);
+					oraFine.setEnabled(true);
+					minutoFine.setEnabled(true);
+					
+				}
+			}
+		});
+
+		
+		
+		
+		
+		
 		JButton btnNewButton = new JButton("RICHIEDI");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+																					//	Contenuto combobox
+				int index=motivazioni.getSelectedIndex();
+				String motivazioneSelezionata = listaMotivazioni[index];
+																					//	Contenuto chooseDate inizio e fine
+				Date dataInseritaInizio = dataInizio.getDate();
+				DateFormat cambioFormatoDataI = DateFormat.getDateInstance(DateFormat.SHORT);
+				String dataI = cambioFormatoDataI.format(dataInseritaInizio);
+				
+				Date dataInseritaFine = dataFine.getDate();
+				DateFormat cambioFormatoDataF = DateFormat.getDateInstance(DateFormat.SHORT);
+				String dataF = cambioFormatoDataF.format(dataInseritaFine);	
+				
+				Boolean checkBoxGiornate = giornateComplete.isSelected();			// contenuto checkbox
+				
+				String oraI, oraF, minutoI, minutoF;
+				
+				if(checkBoxGiornate== true)									//disattivazione combobox --> valori predefiniti
+				{
+					oraI = "00";
+					oraF = "23";
+					minutoI = "00";
+					minutoF = "59";
+				}else														//attivazione delle combobox per la checkbox
+				{
+					oraI = ora[oraInizio.getSelectedIndex()];
+					oraF = ora[oraFine.getSelectedIndex()];
+					minutoI = minuti[minutoInizio.getSelectedIndex()];
+					minutoF = minuti[minutoFine.getSelectedIndex()];
+				}
+				
+				
+				
+			}
+		});
 		btnNewButton.setFont(new Font("Times New Roman", Font.PLAIN, 28));
 		panel_5.add(btnNewButton);
 	}
