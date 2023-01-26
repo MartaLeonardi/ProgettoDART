fc1ea4e1b2e876ceb0133f523314d96f2396a119
cassa <cassa@DESKTOP-8RT5USK.station>
2023-01-26 21:41:20 +0100

    Uscita

--- ProgettoDART/src/Testing/BoundaryTempo.java
+++ ProgettoDART/src/Testing/BoundaryTempo.java
@@ -11,11 +11,12 @@ public class BoundaryTempo {
 		LocalDate d = LocalDate.now();
 		MyTaskCalendario taskCalendario=new MyTaskCalendario();
 		MyTaskStipendio taskStipendio=new MyTaskStipendio();
+		MyTaskUscita taskUscita=new MyTaskUscita();
 		Timer timer1=new Timer();
 		System.out.print("Inizio \n");
 		timer1.schedule(taskCalendario, Date.valueOf(d), 86400000);
 		timer1.schedule(taskStipendio, Date.valueOf(d), 86400000);
-
+		timer1.schedule(taskUscita, 900000);
 	}
 
 }
--- ProgettoDART/src/Testing/MyTaskUscita.java
+++ ProgettoDART/src/Testing/MyTaskUscita.java
@@ -1,5 +1,21 @@
 package Testing;
 
-public class MyTaskUscita {
+import java.time.LocalTime;
+import java.util.TimerTask;
+
+import RilevazionePresenze.UscitaAutomaticaControl;
+
+public class MyTaskUscita extends TimerTask {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		UscitaAutomaticaControl uac = new UscitaAutomaticaControl();
+		LocalTime time = LocalTime.now();
+		if(((time.getHour()%2)==0 && time.getMinute()==15)) {
+			System.out.println("Uscita calcolata");
+			uac.uscitaAuto();
+		} else {System.out.println("Uscita non calcolata");}
+	}
 
 }
